58ea7d8006adc7a89f6edd0d9eb1a6e8883a45df
Nicole Jamilex Diaz Valdez <75482188+Nicole352@users.noreply.github.com>
2025-12-03 18:10:14 -0500

    Cambios Auditoria SuperAdmin

--- src/routes/roles.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { listRolesController } = require('../controllers/roles.controller');
-const { authMiddleware, requireRole } = require('../middleware/auth');
-
-// Permitir acceso a superadmin/admin/administrativo para listar roles
-router.get('/', authMiddleware, requireRole(['superadmin', 'admin', 'administrativo']), listRolesController);
-
-module.exports = router;
